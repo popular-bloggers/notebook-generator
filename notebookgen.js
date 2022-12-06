5d26e38647d344115aff0d0975e6be8efad3f8e9
dshindov <dshindov@protonmail.com>
2022-12-06 12:09:43 +0300

    +XeLaTex +latexmk

--- notebookgen.js
+++ notebookgen.js
@@ -1,132 +1,130 @@
-const fs = require('fs')
-const path = require('path')
-const spawn = require('child_process').spawn
-const through2 = require('through2')
-const tmp = require('tmp')
-const os = require('os')
+const fs = require("fs");
+const path = require("path");
+const spawn = require("child_process").spawn;
+const through2 = require("through2");
+const tmp = require("tmp");
+const os = require("os");
 
-const section = ['\\section{', '\\subsection{', '\\subsubsection{']
+const section = ["\\section{", "\\subsection{", "\\subsubsection{"];
 const extensions = {
-  '.cc': 'C++',
-  '.cpp': 'C++',
-  '.hpp': 'C++',
-  '.c': 'C',
-  '.java': 'Java',
-  '.py': 'Python',
-  '.tex': 'Tex',
-  '.go': 'Golang'
-}
+  ".cc": "C++",
+  ".cpp": "C++",
+  ".hpp": "C++",
+  ".c": "c",
+  ".java": "java",
+  ".py": "python",
+  ".tex": "tex",
+  ".go": "golang",
+};
 
-function walk (_path, depth) {
-  let ans = ''
-  depth = Math.min(depth, section.length - 1)
+function walk(_path, depth) {
+  let ans = "";
+  depth = Math.min(depth, section.length - 1);
   fs.readdirSync(_path).forEach(function (file) {
-    if (file.startsWith('.')) {
-      return // hidden directory
+    if (file.startsWith(".")) {
+      return; // hidden directory
     }
-    const f = path.resolve(_path, file)
-    const stat = fs.lstatSync(f)
+    const f = path.resolve(_path, file);
+    const stat = fs.lstatSync(f);
     if (stat.isDirectory()) {
-      ans += '\n' + section[depth] + file + '}\n' + walk(f, depth + 1)
+      ans += "\n" + section[depth] + file + "}\n" + walk(f, depth + 1);
     } else if (path.extname(f) in extensions) {
-      ans += '\n' + section[depth] + file.split('.')[0] + '}\n'
-      if (path.extname(f) !== '.tex') {
-        ans += `\\begin{lstlisting}[language=${extensions[path.extname(f)]}]\n` + fs.readFileSync(f) + '\\end{lstlisting}\n'
+      ans += "\n" + section[depth] + file.split(".")[0] + "}\n";
+      if (path.extname(f) !== ".tex") {
+        ans +=
+          `\\begin{minted}{${extensions[path.extname(f)]}}\n` +
+          fs.readFileSync(f) +
+          "\\end{minted}\n";
       } else {
-        ans += fs.readFileSync(f)
+        ans += fs.readFileSync(f);
       }
     }
-  })
-  return ans
+  });
+  return ans;
 }
 
 /**
  * pdf must be generated twice in order to generate the table of contents.
  * According to some tests, in windows it must be generated 3 times.
  * */
-function genpdf (ans, texPath, tmpobj, iter) {
-  const tex = spawn('pdflatex', [
-    '-interaction=nonstopmode',
-    texPath
-  ], {
+function genpdf(ans, texPath, tmpobj) {
+  const tex = spawn("latexmk", ["-xelatex", "-shell-escape", texPath], {
     cwd: tmpobj.name,
-    env: process.env
-  })
+    env: process.env,
+  });
 
-  tex.on('error', function (err) {
-    console.error(err)
-  })
+  tex.on("error", function (err) {
+    console.error(err);
+  });
 
-  tex.on('exit', function (code, signal) {
-    const outputFile = texPath.split('.')[0] + '.pdf'
+  tex.on("exit", function (code, signal) {
+    const outputFile = texPath.split(".")[0] + ".pdf";
     fs.access(outputFile, function (err) {
       if (err) {
-        return console.error('Not generated ' + code + ' : ' + signal)
+        return console.error("Not generated " + code + " : " + signal);
       }
-      if (iter === 0) {
-        const s = fs.createReadStream(outputFile)
-        s.pipe(ans)
-        s.on('close', function () {
-          tmpobj.removeCallback()
-        })
-      } else {
-        genpdf(ans, texPath, tmpobj, iter - 1)
-      }
-    })
-  })
+      const s = fs.createReadStream(outputFile);
+      s.pipe(ans);
+      s.on("close", function () {
+        tmpobj.removeCallback();
+      });
+    });
+  });
 }
 
-function pdflatex (doc) {
-  const tmpobj = tmp.dirSync({ unsafeCleanup: true })
-  const texPath = path.join(tmpobj.name, '_notebook.tex')
+function latexmk(doc) {
+  const tmpobj = tmp.dirSync({ unsafeCleanup: true });
+  const texPath = path.join(tmpobj.name, "_notebook.tex");
 
-  const ans = through2()
-  ans.readable = true
-  const input = fs.createWriteStream(texPath)
-  input.end(doc)
-  input.on('close', function () {
-    const iters = process.platform === 'win32' ? 2 : 1
-    genpdf(ans, texPath, tmpobj, iters)
-  })
+  const ans = through2();
+  ans.readable = true;
+  const input = fs.createWriteStream(texPath);
+  input.end(doc);
+  input.on("close", function () {
+    genpdf(ans, texPath, tmpobj);
+  });
 
-  return ans
+  return ans;
 }
 
-function normalizeUnixStyle (currentPath) {
-  if (os.type() === 'Windows_NT') {
-    return currentPath.replace(/\\/g, '/')
-  }
-  return currentPath
-}
+// function normalizeUnixStyle(currentPath) {
+//   if (os.type() === "Windows_NT") {
+//     return currentPath.replace(/\\/g, "/");
+//   }
+//   return currentPath;
+// }
 
-function templateParameter (parameter) {
-  return `\${${parameter}}`
+function templateParameter(parameter) {
+  return `\${${parameter}}`;
 }
 
 module.exports = function (_path, options) {
-  options.output = options.output || './notebook.pdf'
-  options.author = options.author || ''
-  options.initials = options.initials || ''
+  options.output = options.output || "./notebook.pdf";
+  options.author = options.author || "";
+  options.initials = options.initials || "";
 
-  if (!options.size.endsWith('pt')) options.size += 'pt'
-  if (options.image) {
-    options.image = normalizeUnixStyle(path.resolve(options.image))
-    options.image = '\\centering{\\includegraphics[width=3.5cm]{' + options.image + '}}'
-  } else {
-    options.image = ''
-  }
+  if (!options.size.endsWith("pt")) options.size += "pt";
+  // if (options.image) {
+  //   options.image = normalizeUnixStyle(path.resolve(options.image));
+  //   options.image =
+  //     "\\centering{\\includegraphics[width=3.5cm]{" + options.image + "}}";
+  // } else {
+  //   options.image = "";
+  // }
 
-  let template = fs.readFileSync(path.join(__dirname, 'template_header.tex')).toString()
+  let template = fs
+    .readFileSync(path.join(__dirname, "template_header.tex"))
+    .toString();
   template = template
-    .replace(templateParameter('author'), options.author)
-    .replace(templateParameter('initials'), options.initials)
-    .replace(templateParameter('fontSize'), options.size)
-    .replace(templateParameter('columns'), options.columns)
-    .replace(templateParameter('paper'), options.paper)
-    .replace(templateParameter('image'), options.image)
+    .replaceAll(templateParameter("author"), options.author)
+    .replaceAll(templateParameter("initials"), options.initials)
+    .replaceAll(templateParameter("fontSize"), options.size)
+    .replaceAll(templateParameter("columns"), options.columns)
+    .replaceAll(templateParameter("paper"), options.paper)
+    .replaceAll(templateParameter("image"), options.image);
 
-  template += walk(_path, 0)
-  template += '\\end{multicols}\n'
-  template += '\\end{document}'
-  pdflatex(template).pipe(fs.createWriteStream(options.output))
-}
+  template += walk(_path, 0);
+  template += "\\end{multicols}\n";
+  template += "\\end{document}";
+  latexmk(template).pipe(fs.createWriteStream(options.output));
+};
